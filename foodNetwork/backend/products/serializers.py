08e30db761ace94a297f129d67732029161717d4
hmoomin <hollie3.baker@live.uwe.ac.uk>
2026-03-21 10:32:06 +0000

    fixed the add product

--- foodNetwork/backend/products/serializers.py
+++ foodNetwork/backend/products/serializers.py
@@ -5,6 +5,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+        read_only_fields = ["producer"]
 
 class EducationalContentSerializer(serializers.ModelSerializer):
     class Meta:
