d8bcc986ca60f3e2ca5b65888514de3b8c16c0d6
hmoomin <hollie3.baker@live.uwe.ac.uk>
2026-05-16 15:25:29 +0100

    fixed the search bar

--- foodNetwork/backend/products/views.py
+++ foodNetwork/backend/products/views.py
@@ -43,9 +43,8 @@ class ProductListView(generics.ListAPIView):
                 Q(description__icontains=search) |
                 Q(origin_farm__icontains=search) |
                 Q(producer__username__icontains=search)|
-                Q(organic__icontains=search)|
-                Q(category__icontains=search)|
-                Q(max_distance__icontains=search)
+                Q(is_organic__icontains=search)|
+                Q(category__icontains=search)
             )
 
         if producer:
